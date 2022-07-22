d16ebebd41e0d7690de82dbc09cb564fa09423d4
Rashin-Aeini <aeini.rashin@gmail.com>
2022-07-22 12:59:36 +0200

    Create Mapper For User and Update DetailsUser

---
+++ LibraryManager/Models/Mappers/UserProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using LibraryManager.Models.Domains;
+using LibraryManager.Models.ViewModels.User;
+
+namespace LibraryManager.Models.Mappers
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<CreateUser, User>()
+                .ForMember(user => user.FirstName, config => config.MapFrom(create => create.FirstName))
+                .ForMember(user => user.LastName, config => config.MapFrom(create => create.LastName))
+                .ForMember(user => user.UserName, config => config.MapFrom(create => create.UserName));
+
+            CreateMap<User, DetailsUser>()
+                .ForMember(details => details.Id, config => config.MapFrom(user => user.Id))
+                .ForMember(details => details.FirstName, config => config.MapFrom(user => user.FirstName))
+                .ForMember(details => details.LastName, config => config.MapFrom(user => user.LastName))
+                .ForMember(details => details.UserName, config => config.MapFrom(user => user.UserName))
+                .ForMember(details => details.Confirm, config => config.MapFrom(user => user.EmailConfirmed));
+        }
+    }
+}
